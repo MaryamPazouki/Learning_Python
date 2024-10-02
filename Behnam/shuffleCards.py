3ff017890d5876b3b3fb2a5e953ab9f8e3960aa4
behnamkh127 <137055604+behnamkh127@users.noreply.github.com>
2024-10-02 16:36:44 +0000

    completed shuffleCards.py

--- Behnam/shuffleCards.py
+++ Behnam/shuffleCards.py
@@ -4,7 +4,17 @@ ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
 
 suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
-rank = random.randrange(0, len(ranks))
-suit = random.randrange(0, len(suits))
-
-print(ranks[rank] + " of " + suits[suit])
+# rank = random.randrange(0, len(ranks))
+# suit = random.randrange(0, len(suits))
+deck = []
+for suit in suits:
+    for rank in ranks:
+        card = rank + ' of ' + suit
+        deck += [card]
+for i in range(len(deck)):
+    r = random.randrange(0, i + 1)
+    temp = deck[i]
+    deck[i] = deck[r]
+    deck[r] = temp
+for i in deck:
+    print(i)
