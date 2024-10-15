c2b6f0ae74c03d5ec07afd584a4e49673bbd98d4
behnamkh127 <137055604+behnamkh127@users.noreply.github.com>
2024-10-15 16:20:35 +0000

    20

---
+++ Behnam/twentyQuestion.py
@@ -0,0 +1,19 @@
+import sys
+import random
+# import stdio
+
+n = 20
+number = random.randint(1, 1000000)
+print("I'm thinking for a number between 1 to 1000000 can you find it?")
+for i in range(n):
+    print("what is your guest? ")
+    guess = int(input())
+    if guess > number:
+        print("too high")
+    elif guess < number:
+        print("too low")
+    elif guess == number:
+        print("You win")
+        break
+if i == n:
+    print("You lose")
